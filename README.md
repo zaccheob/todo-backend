9197b19e2317fdf701a907908396d991471c1686
Zaccheo Bagnati <zaccheob@gmail.com>
2019-03-11 22:49:58 +0100

    Empty spring application

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # todo-backend
 Sample implementation of a todo list backend service
+
+To run the application:
+`mvn spring-boot:run`
---
+++ src/test/java/org/zaccheo/todo/TodoApplicationTests.java
@@ -0,0 +1,25 @@
+package org.zaccheo.todo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void contextLoads() {
+        assertThat(mvc).isNotNull();
+    }
+}
